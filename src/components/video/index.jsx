a750a316a1fd47977d918a78f280ee4e95e8f5f9
Balint Kleri <b@balintbrews.com>
2025-12-03 15:15:39 +0100

    fix(components:video): remove empty caption track

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -1,8 +1,8 @@
 const Video = ({ video }) => {
   return (
+    // eslint-disable-next-line jsx-a11y/media-has-caption
     <video className="w-full" controls>
       <source src={video.src} />
-      <track kind="captions" srcLang="en" label="English" default />
     </video>
   );
 };
